c92e5f973bb92df87ff70d2ceb8484d48a72cb28
tarunrajesh07 <152465467+tarunrajesh07@users.noreply.github.com>
2025-01-21 19:33:00 -0800

    DefaultCurves: make UNIT_S_CURVE final
    A mutable public static field on a constants holder class invites
    accidental reassignment by any consumer; it was always meant to be a
    constant.

--- utils/DefaultCurves.java
+++ utils/DefaultCurves.java
@@ -5,7 +5,7 @@ import com.roboracers.topgear.planner.CubicBezierCurve;
 
 public class DefaultCurves {
 
-    public static CubicBezierCurve UNIT_S_CURVE = new CubicBezierCurve(
+    public static final CubicBezierCurve UNIT_S_CURVE = new CubicBezierCurve(
             new Vector2d(0,0), new Vector2d(1,0),
             new Vector2d(0,1), new Vector2d(1,1));
 }
